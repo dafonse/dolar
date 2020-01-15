d56350ab7db6769a6b11d665f58516cae4860187
André Fonseca <11467925+dafonse@users.noreply.github.com>
2020-01-15 16:44:32 -0300

    Response variable
    Initializing response variable and testing if it differs for None.

--- dolar.py
+++ dolar.py
@@ -7,6 +7,8 @@ API_HOST = "api.promasters.net.br"
 PATH = "/cotacao/v1/valores"
 QUERY = "moedas="
 
+response = None
+
 def get_value(moeda):
     request_string = "{}?{}{}".format(PATH, QUERY, moeda)
         
@@ -17,24 +19,25 @@ def get_value(moeda):
     except Exception as e:
         print("Ocorreu um erro ao conectar ao servidor:", e)
         raise e
-
-    if response.status != 200:
-        print("O servidor retornou uma resposta inesperada:", response.status, response.reason)
-        raise Exception("Invalid response")
-
-    try:
-        json_data = response.read()
-        data = json.loads(json_data)
-
-        if data['status'] == False:
-            print("O servidor não conseguiu processar a requisição:", data['retorno'])
-            raise Exception("Invalid response")
         
-        cotacao = data['valores'][moeda]['valor']
-        atualizacao = data['valores'][moeda]['ultima_consulta']
-    except Exception as e:
-        print("Ocorreu um erro ao decodificar a resposta do servidor:", e)
-        raise e
+    if response != None:
+        if response.status != 200:
+            print("O servidor retornou uma resposta inesperada:", response.status, response.reason)
+            raise Exception("Invalid response")
+
+        try:
+            json_data = response.read()
+            data = json.loads(json_data)
+
+            if data['status'] == False:
+                print("O servidor não conseguiu processar a requisição:", data['retorno'])
+                raise Exception("Invalid response")
+        
+            cotacao = data['valores'][moeda]['valor']
+            atualizacao = data['valores'][moeda]['ultima_consulta']
+        except Exception as e:
+            print("Ocorreu um erro ao decodificar a resposta do servidor:", e)
+            raise e
     
     return (cotacao, atualizacao)
 
